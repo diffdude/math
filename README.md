0499d3fa1aea6b032f2d5171146bbb58f4aab879
diffdude <jmlw1@proton.me>
2025-09-12 11:09:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#math
+shell scripting practice to take numeric inputs and perform intended operations
+
+This started out as subtract.sh which would take two numbers from user input
+and pass them into variables to subtract num2 from num1. I added addition
+functionality and division functionality, and ran into some difficulty with
+correctly using git. The subtract repository has been deleted, and the code has
+been copied to math.sh to create a new repository.
+
+As of the time of writing, the division function only returns integers or floats
+with 13 decimal spaces.
+
+This is meant to be a shell scripting learning exercise. If there is something
+useful for you, you are free to copy and paste and modify any section of code.
---
+++ math.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# determine user intent to add or subtract
+echo "Would you like to [add], [subtract] or [divide]?"
+# takes input as variable called "intent"
+read intent
+# if intent is to subtract, run core subtract code
+# pipes intent variable into a command to force input into lowercase
+if [ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "subtract" ]; then
+	echo "Enter a number to reduce"
+		read num1
+	echo "Enter a number to subtract"
+		read num2
+# creates [diff]erence variable as num1 - num2 and pipes it into basic calculator to allow floats
+	diff=$(echo "$num1 - $num2" | bc)
+	echo "The difference is $diff"
+# if intent is to add, run add code
+elif [ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "add" ]; then
+	echo "Enter a number to increase"
+		read num1
+	echo "Enter a number to add"
+		read num2
+# creates sum variable as num1 + num2 and pipes it into basic calculator to allow floats
+	sum=$(echo "$num1 + $num2" | bc)
+	echo "The sum is $sum"
+# if intent is to divide, run code to divide
+elif [ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "divide" ]; then
+	echo "Enter a numerator to divide"
+		read num1
+	echo "Enter a denominator to divide by"
+		read num2
+# creates result variable as num1 / num2 and pipes it into basic calculator to allow division
+# scale=13 to allow 13 decimal places
+	result13=$(echo "scale=13; $num1 / $num2" | bc)
+	resultInt=$(echo "$num1 / $num2" | bc)
+# checks if $result is trailed by 13 zeros
+	if [ $result13 == $resultInt.0000000000000 ]; then
+# if so, [quot]ient is integer returned by bc division
+		quot=$resultInt
+# If result is not integer, $quot = $result13
+	else
+		quot=$result13
+	fi
+	echo "The quotient is $quot"
+# if intent is not supported, promise future functionality
+else
+	echo "More functionality coming soon."
+#end of if/else statement
+fi
+exit 0
