d7f8146f932b3df15f40e4e30c6729a4b08c23ff
diffdude <jmlw1@proton.me>
2025-09-17 17:52:02 -0500

    Updated math.sh to match mathdev.sh

--- math.sh
+++ math.sh
@@ -3,153 +3,142 @@ nonnum="Please use only numeric inputs"
 start() {
 	printf "What would you like to do?\n[add] [subtract] [divide] [multiply] [exponent]\n"
 	read intent
+	intent=$(echo "$intent" | tr '[:upper:]' '[:lower:]')
 	code
 }
 continoo_check(){
-	if
-		[[ -z "$num1" ]]; then
-			input1
-	else
-		input2
-	fi
+	[ -z "$num1" ] && input1
+	input2
 }
 validate_numeric() {
-	if
-		[[ $num1 =~ [a-zA-Z] ]]; then
-			echo $nonnum
-			exit
-	elif
-		[[ $num2 =~ [a-zA-Z] ]]; then
-			echo $nonnum
-			exit
-	fi
+	case $num1 in
+		*[a-zZ-A]*)
+			echo $nonnum && exit ;;
+	esac
+	case $num2 in
+		*[a-zZ-A]*)
+			echo $nonnum && exit ;;
+	esac
+}
+result_normalize(){
+	result=$(echo "$result" | sed 's/\.0*$//' | sed 's/\.$//')
 }
 continoo(){
-	printf "Would you like to perform an operation on $result? [Y/N]
-"
-	read continue
-	if
-		[[ "$(echo "$continue" | tr '[:upper:]' '[:lower:]')" = "y" ]]; then
-			reassign
-			start
-	else
-		exit
-	fi
+	printf "Would you like to perform an operation on $result? [Y/N] \n"
+	read contin
+	contin=$(echo "$contin" | tr '[:upper:]' '[:lower:]')
+	case $contin in
+		"y")
+			reassign && start ;;
+		*)
+			exit ;;
+	esac
 }
 reassign() {
 	num1=$result
 }
 code() {
-if
-	[[ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "subtract" ]]; then
-		input2() {
-			echo "Enter a number to subtract"
-			read num2
-		}
-		input1() {
-			echo "Enter a number to reduce"
-			read num1
-			input2
-		}
-	continoo_check
-	validate_numeric
-	result=$(echo "$num1 - $num2" | bc)
-	printf "The difference is $result \n"
-	continoo
-elif
-	[[ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "add" ]]; then
-		input2() {
-			echo "Enter a number to add"
-			read num2
-		}
-		input1() {
-			echo "Enter a number to increase"
-			read num1
-			input2
-		}
-	continoo_check
-	validate_numeric
-	result=$(echo "$num1 + $num2" | bc)
-	printf "The sum is $result \n"
-	continoo
-elif
-	[[ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "divide" ]]; then
-		input2() {
-			echo "Enter a denominator to divide by"
-			read num2
-		}
-		input1() {
-			echo "Enter a numerator to divide"
-			read num1
-			input2
-		}
-	continoo_check
-	validate_numeric
-	if
-		[[ $num2 = 0 ]]; then
-			echo "Cannot divide by zero"
+	case $intent in
+		"add")
+			input2() {
+				echo "Enter a number to add to $num1"
+				read num2
+			}
+			input1() {
+				echo "Enter a number to increase"
+				read num1
+			}
+			continoo_check
+			validate_numeric
+			result=$(echo "$num1 + $num2" | bc)
+			result_normalize
+			printf "The sum is $result \n"
+			continoo ;;
+		"subtract")
+			input2() {
+				echo "Enter a number to subtract from $num1"
+				read num2
+			}
+			input1() {
+				echo "Enter a number to reduce"
+				read num1
+			}
+			continoo_check
+			validate_numeric
+			result=$(echo "$num1 - $num2" | bc)
+			result_normalize
+			printf "The difference is $result \n"
+			continoo ;;
+		"multiply")
+			input2() {
+				echo "Enter a number to multiply $num1 by"
+				read num2
+			}
+			input1() {
+				echo "Enter a number to multiply"
+				read num1
+			}
+			continoo_check
+			validate_numeric
+			result=$(echo "$num1 * $num2" | bc)
+			result_normalize
+			printf "The product is $result \n"
+			continoo ;;
+		"divide")
+			input2() {
+				echo "Enter a denominator to divide $num1 by"
+				read num2
+			}
+			input1() {
+				echo "Enter a numerator to divide"
+				read num1
+			}
+			continoo_check
+			validate_numeric
+			if [[ $num2 = 0 ]]; then
+				echo "Cannot divide by zero"
+				exit
+			else
+				quotient_13=$(echo "scale=13; $num1 / $num2" | bc)
+				quotient_integer=$(echo "$num1 / $num2" | bc)
+			fi
+			if [[ $quotient_13 = $(echo "scale=13; $quotient_integer"| bc) ]]; then
+				result=$quotient_integer
+			else
+				result=$(echo "$quotient_13")
+			fi
+			result_normalize
+			printf "The quotient is $result \n"
+			continoo ;;
+		"exponent")
+			input2() {
+				echo "Enter an exponent for $num1"
+				read num2
+			}
+			input1() {
+				echo "Enter a base to be exponentiated"
+				read num1
+			}
+			continoo_check
+			validate_numeric
+			case 0 in
+				$num1)
+					printf "The power is 0\n" && result=0 && continoo ;;
+				$num2)
+					printf "The power is 1\n" && result=1 && continoo ;;
+			esac
+			result=$(echo "$num1 + $num2" | bc -l)
+			case $result in
+				*.*) result=$(echo "e($num2 * l($num1))" | bc -l) ;;
+				*) result=$(echo "$num1 ^ $num2" | bc -l) ;;
+			esac
+			result_normalize
+			printf "The power is $result \n"
+			continoo ;;
+		*)
+			echo "More functionality coming soon."
 			exit
-	else
-		quotient_13=$(echo "scale=13; $num1 / $num2" | bc)
-		quotient_integer=$(echo "$num1 / $num2" | bc)
-	fi
-	if
-		[[ $quotient_13 = $quotient_integer.0000000000000 ]]; then
-			result=$quotient_integer
-	else
-		result=$(echo "$quotient_13" | sed 's/0*$//')
-	fi
-	printf "The quotient is $result \n"
-	continoo
-elif [[ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "multiply" ]]; then
-	input2() {
-		echo "Enter another number to multiply"
-		read num2
-		}
-	input1() {
-		echo "Enter a number to multiply"
-		read num1
-		input2
-	}
-	continoo_check
-	validate_numeric
-	result=$(echo "$num1 * $num2" | bc)
-	printf "The product is $result \n"
-	continoo
-elif
-	[[ "$(echo "$intent" | tr '[:upper:]' '[:lower:]')" = "exponent" ]]; then
-		input2() {
-			echo "Enter an exponent"
-			read num2
-		}
-		input1() {
-			echo "Enter a base to be exponentiated"
-			read num1
-			input2
-		}
-	continoo_check
-	validate_numeric
-	if
-		[[ $num1 = 0 ]]; then
-			printf "The power is 0\n"
-			exit
-	elif
-		[[ $num2 = 0 ]]; then
-			printf "The power is 1\n"
-			exit
-#	power=$(echo "$num1 ^ $num2" | bc) does not accept floats as exponents
-	elif
-		[[ $(echo "$num1 + $num2" | bc) =~ \.[0-9]+ ]]; then
-			result=$(echo "e($num2 * l($num1))" | bc -l)
-	else
-		result=$(echo "$num1 ^ $num2" | bc)	
-	fi
-	printf "The power is $result \n"
-	continoo
-else
-	echo "More functionality coming soon."
-	exit
-fi
+	esac
 }
 start
 exit
